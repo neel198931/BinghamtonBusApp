e53e4227e4659c77eecabba3d06e3d9b56efa756
Chirag Gosalia <chirag.gosalia@outlook.com>
2015-04-17 21:43:44 -0400

    Updated Test Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,71 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+bus_list = ["LRS", "Oakdale", "WS", "RES", "DCR"]
+
+bus_list.each do |bus|
+		Bus.create( name: bus)
+	end
+
+# Generate test values for Stop Table
+stop_list = ["Downtown", "Resident", "Dilingers", "Price Chopper", "Walmart"]
+long = 123
+lat = 456
+stop_list.each do |stop|
+		Stop.create(name: stop, longitude: long, latitude: lat)
+		long += 50
+		lat += 50
+end
+
+#Generate test values for Schedule table
+
+arrival_list = ["2:00:00", "3:00:00", "4:00:00"]
+arrival_list1= ["2:30:00","3:30:00", "4:30:00"]
+arrival_list2= ["3:00:00", "4:00:00", "5:00:00"]
+arrival_list3= ["3:30:00","4:30:00","5:30:00"]
+
+busId = 1
+stopId = 1
+arrival_list.each do |time|
+	sched = Schedule.new
+	sched.arrival = time
+    bus = Bus.find(busId)
+    stop = Stop.find(stopId)
+    bus.schedules << sched
+    stop.schedules << sched
+end
+
+
+
+busId = 2
+stopId = 1
+arrival_list1.each do |time|
+	sched = Schedule.new
+	sched.arrival = time
+    bus = Bus.find(busId)
+    stop = Stop.find(stopId)
+    bus.schedules << sched
+    stop.schedules << sched
+end
+
+busId = 1
+stopId = 3
+arrival_list2.each do |time|
+	sched = Schedule.new
+	sched.arrival = time
+    bus = Bus.find(busId)
+    stop = Stop.find(stopId)
+    bus.schedules << sched
+    stop.schedules << sched
+end
+
+busId = 2
+stopId = 3
+arrival_list3.each do |time|
+	sched = Schedule.new
+	sched.arrival = time
+    bus = Bus.find(busId)
+    stop = Stop.find(stopId)
+    bus.schedules << sched
+    stop.schedules << sched
+end
